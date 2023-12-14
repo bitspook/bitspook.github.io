4647cee0dc011ca09855d6b9607846cfcbf45d24
Charanjit Singh <bitspook@proton.me>
2023-12-14 15:02:04 +0100

    refactor: cleaner code to publish blog posts

--- scratch.lisp
+++ scratch.lisp
@@ -9,7 +9,8 @@
 
 (defparameter *base-dir* (asdf:system-relative-pathname :in.bitspook.website ""))
 
-(defparameter *notes*
+;; expensive operations stored in top-level variables for caching
+(defparameter *denote-posts*
   (let ((notes-provider (make-instance 'denote-provider)))
     (mapcar
      (op (from _ 'blog-post :author *author*))
@@ -18,80 +19,55 @@
 (defparameter *local-org-files*
   (let ((org-provider (make 'org-file-provider)))
     (provide-all org-provider (path-join *base-dir* "content/"))))
+;; end expensive operations
 
-(defun local-org-file-to-post (file)
-  (let ((post (from file 'blog-post :author *author*)))
-    ;; Set filename as slug if no slug is explicitly provided
-    (when (not (@ (org-file-metadata file) "slug"))
-      (setf (post-slug post)
-            (first (str:split "." (file-namestring (org-file-filepath file))))))
-    post))
-
-(defparameter *local-posts*
-  (mapcar
-   #'local-org-file-to-post
-   (remove-if-not
-    (op (str:containsp "/blog/" (org-file-filepath _)))
-    *local-org-files*)))
-
-(defparameter *poems*
-  (mapcar
-   #'local-org-file-to-post
-   (remove-if-not
-    (op (str:containsp "/poems/" (org-file-filepath _)))
-    *local-org-files*)))
-
-(defparameter *talks*
-  (mapcar
-   #'local-org-file-to-post
-   (remove-if-not
-    (op (str:containsp "/talks/" (org-file-filepath _)))
-    *local-org-files*)))
-
-(defparameter *about-me*
-  (org-file-to-blog-post
-   (find-if (op (str:containsp "/about.org" (org-file-filepath _)))
-            *local-org-files*)))
-
-(defparameter *blog-posts*
-  (let* ((all-posts (append *local-posts* *notes*))
-         (no-drafts (remove-if
-                     (op (or (find "draft" (post-tags _1) :test #'equal)
-                             (find "micro" (post-tags _1) :test #'equal)))
-                     all-posts)))
-    no-drafts))
+(defparameter *categorized-posts*
+  (labels ((local-org-file-to-post (file)
+             (let ((post (from file 'blog-post :author *author*)))
+               ;; Set filename as slug if no slug is explicitly provided
+               (when (not (@ (org-file-metadata file) "slug"))
+                 (setf (post-slug post)
+                       (first (str:split "." (file-namestring (org-file-filepath file))))))
+               post)))
+    (let* ((categorized-posts
+             (let* ((base-dir (namestring (path-join *base-dir* "content/")))
+                    (posts (dict)))
+               (loop :for file :in *local-org-files*
+                     :for category := (str:substring
+                                       0 -1
+                                       (directory-namestring
+                                        (str:replace-first base-dir "" (org-file-filepath file))))
+                     :do (let ((cat-posts (@ posts category)))
+                           (setf (@ posts category) (append1 cat-posts (local-org-file-to-post file)))))
+               posts))
+           (all-posts (append (@ categorized-posts "blog")
+                              *denote-posts*))
+           (published-posts (remove-if
+                             (op (or (find "draft" (post-tags _1) :test #'equal)
+                                     (find "micro" (post-tags _1) :test #'equal)))
+                             all-posts)))
+      (setf (@ categorized-posts "blog") published-posts)
+      categorized-posts)))
 
 (defun build ()
   (let* ((www (path-join *base-dir* "docs/"))
          (static (path-join *base-dir* "src/static/"))
          (*print-pretty* nil)
-         (asset-pub (make 'asset-publisher :dest www))
-         (blog-pub (make 'blog-post-publisher
-                         :asset-pub asset-pub
-                         :dest (path-join www "blog/")))
-         (poems-pub (make 'blog-post-publisher
-                          :asset-pub asset-pub
-                          :dest (path-join www "poems/")))
-         (talks-pub (make 'blog-post-publisher
-                          :asset-pub asset-pub
-                          :dest (path-join www "talks/")))
-         (page-pub (make 'blog-post-publisher
-                         :asset-pub asset-pub
-                         :dest www)))
+         (asset-pub (make 'asset-publisher :dest www)))
 
     (uiop:delete-directory-tree www :validate t :if-does-not-exist :ignore)
 
     (publish asset-pub :content static)
 
-    (publish page-pub :post *about-me* :layout (make 'blog-post-w :post *about-me*))
-
-    (loop
-      :for (publisher posts) :in `((,blog-pub ,*blog-posts*)
-                                   (,poems-pub ,*poems*)
-                                   (,talks-pub ,*talks*))
-      :do (loop :for post :in posts
-                :do (let ((layout-w (make 'blog-post-w :post post)))
-                      (publish publisher :post post :layout layout-w))))))
+    (do-hash-table (category posts *categorized-posts*)
+      (let ((publisher (make 'blog-post-publisher
+                             :asset-pub asset-pub
+                             :dest (base-path-join www (if (str:emptyp category) ""
+                                                           (str:concat category "/"))))))
+        (loop :for post :in posts
+              :do (publish publisher
+                           :post post
+                           :layout (make 'blog-post-w :post post)))))))
 
 (build)
 
